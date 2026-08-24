11311e1c26f49ab814b66bb4e9d3f06e6d8b1f13
MSurya19 <suryammcd19@gmail.com>
2026-08-24 19:52:31 +0530

    Configure SKYRA frontend for production

---
+++ frontend/js/config.js
@@ -0,0 +1,24 @@
+﻿"use strict";
+
+(() => {
+
+    const isLocal =
+        window.location.hostname === "localhost" ||
+        window.location.hostname === "127.0.0.1";
+
+    const backendBaseUrl =
+        isLocal
+            ? "http://localhost:5000"
+            : "https://skyra-backend-wpzm.onrender.com";
+
+    window.SKYRA_CONFIG = Object.freeze({
+
+        API_BASE_URL:
+            backendBaseUrl + "/api",
+
+        REALTIME_BASE_URL:
+            backendBaseUrl
+
+    });
+
+})();
